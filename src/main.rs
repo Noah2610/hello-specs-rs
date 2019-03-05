4facbc5aec0fe49eb10b83dc903a840d5097d6fd
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2019-03-05 14:34:23 +0100

    Chapter 04: Resources

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,30 @@ extern crate specs;
 
 mod components;
 mod systems;
+mod resources;
 
 use specs::{ World, Builder, RunNow, DispatcherBuilder };
 use self::components::*;
 use self::systems::*;
+use self::resources::*;
 
 fn main() {
+    let mut world = world();
+    run(&mut world);
+}
+
+fn world() -> World {
+    // INITIALIZE
     let mut world = World::new();
+
+    // ADD RESOURCES
+    world.add_resource(DeltaTime(0.05));
+
+    // REGISTER COMPONENTS
     world.register::<Position>();
     world.register::<Velocity>();
 
+    // CREATE ENTITIES
     world
         .create_entity()
         .with(Position { x: 4.0, y: 7.0 })
@@ -22,9 +36,12 @@ fn main() {
         .with(Velocity { x: 0.1, y: 0.2 })
         .build();
 
-    // let mut hello_system = HelloSystem;
-    // hello_system.run_now(&world.res);
-    // world.maintain();
+    world
+}
+
+fn run(world: &mut World) {
+    use std::thread::sleep;
+    use std::time::Duration;
 
     let mut dispatcher = DispatcherBuilder::new()
         .with(HelloSystem, "hello_system",  &[])
@@ -32,5 +49,14 @@ fn main() {
         .with(HelloSystem, "hello_updated", &["update_pos"])
         .build();
 
-    dispatcher.dispatch(&mut world.res);
+    for i in 0 .. 10 {
+        println!("ITERATION {}", i);
+
+        dispatcher.dispatch(&mut world.res);
+        world.maintain();
+        let mut dt = world.write_resource::<DeltaTime>();
+        *dt = DeltaTime(0.6);
+
+        sleep(Duration::from_millis(500));
+    }
 }
--- src/systems/update_pos.rs
+++ src/systems/update_pos.rs
@@ -1,16 +1,21 @@
-use specs::{ System, ReadStorage, WriteStorage, Join };
+use specs::{ System, ReadStorage, WriteStorage, ReadExpect, Join };
 use crate::components::*;
+use crate::resources::DeltaTime;
 
 pub struct UpdatePos;
 
 impl<'a> System<'a> for UpdatePos {
-    type SystemData = (ReadStorage<'a, Velocity>,
+    type SystemData = (ReadExpect<'a, DeltaTime>,
+                       ReadStorage<'a, Velocity>,
                        WriteStorage<'a, Position>);
 
-    fn run(&mut self, (vel, mut pos): Self::SystemData) {
+    fn run(&mut self, data: Self::SystemData) {
+        let (dt, vel, mut pos) = data;
+        let dt = dt.0;
+
         for (vel, pos) in (&vel, &mut pos).join() {
-            pos.x += vel.x * 0.05;
-            pos.y += vel.y * 0.05;
+            pos.x += vel.x * dt;
+            pos.y += vel.y * dt;
         }
     }
 }
